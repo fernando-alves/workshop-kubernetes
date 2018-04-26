70999e59541cdb813c8614eda035388e6ffdcce3
Fernando <fernandoasfj@gmail.com>
2018-04-26 15:04:50 -0300

    Provision cluster

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 
 function display_usage {
   echo "Usage: $(basename "$0") <command>"
-  echo ' - build     Builds app'
+  echo ' - build       Builds app'
+  echo ' - provision   Creates/updates kubernetes cluster on GCP'
   exit 1
 }
 
@@ -12,11 +13,31 @@ function build {
   docker-compose build
 }
 
+function is_cluster_password_set {
+  [[ -f "secret.tfvars" ]]
+}
+
+function provision {
+  if ! is_cluster_password_set ; then
+    echo "Error: cluster password has to be defined before provisioning"
+    echo "Please use a secret.tfvars file"
+    exit 1
+  fi
+
+  pushd terraform
+    terraform init
+    terraform apply -var-file="../secret.tfvars"
+  popd
+}
+
 readonly command="${1:-}"
 
 case "$command" in
   build)
     build
+    ;;
+  provision)
+    provision
     ;;
   *)
     display_usage
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,32 @@
 provider "google" {
-  credentials = ""
-  project      = ""
-  region       = "europe-west1"
+  project = "k8s-worshop"
+  region  = "europe-west1"
+  version = "1.10"
+}
+
+resource "google_container_cluster" "workshop-cluster" {
+  name = "workshop-cluster"
+  zone = "europe-west1-c"
+  initial_node_count = 1
+
+  additional_zones = [
+    "europe-west1-d"
+  ]
+
+  master_auth {
+    username = "admin"
+    password = "${var.cluster_password}"
+  }
+
+  node_config {
+    machine_type = "n1-standard-2"
+    disk_size_gb = "100"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_write",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring"
+    ]
+  }
 }
